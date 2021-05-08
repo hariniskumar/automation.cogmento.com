65335188c89cf81b5ee283d50784b40109170532
Harini Kumar <harini.kumar.email@gmail.com>
2021-05-07 21:05:23 -0700

    bring back PageFactory till refactoring all associated classes using PageFactory

--- src/test/java/com/cogmento/pageObjects/GenericPage.java
+++ src/test/java/com/cogmento/pageObjects/GenericPage.java
@@ -4,6 +4,7 @@
 package com.cogmento.pageObjects;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 /**
  * @author Harini
@@ -14,7 +15,7 @@ public class GenericPage {
 
 	public GenericPage(WebDriver driver) {
 		this.driver = driver;
-		// PageFactory.initElements(driver, this);
+		PageFactory.initElements(driver, this);
 	}
 
 	public WebDriver getDriver() {
